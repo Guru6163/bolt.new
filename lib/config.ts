ec35b8415bdd23a24e531882666213a4ef1e826d
GuruF <gprasath20@gmail.com>
2025-10-29 15:33:01 +0530

    udpate be url

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://localhost:3000/api' 
+  ? 'https://bolt-new-eta-neon.vercel.app/api' 
   : 'http://localhost:3000/api';
